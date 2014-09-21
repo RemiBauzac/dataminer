64a4847586879e4d790b6856b0f9c1b252ef3d2d
Remi Bauzac <rbauzac@free.fr>
2014-09-21 23:10:37 +0200

    Add csv loading error test

--- Tests/newcsv.lua
+++ Tests/newcsv.lua
@@ -12,3 +12,10 @@ function Test_modified_load()
   csv:remove(l)
   if csv == sample_miner then error('Error in CSV loading') end
 end
+
+function Test_load_error()
+  local csv, err = pcall(miner.new, 'sample_error.csv', 'csv', ',')
+  if not err:find('CSV parsing error at line 3, column 5') then
+    error('Error in CSV loading, errror awaited')
+  end
+end
